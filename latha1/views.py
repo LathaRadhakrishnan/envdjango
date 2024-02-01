55c8e1b33f3151914c46094cac024050cd00638f
LathaRadhakrishnan <114051430+LathaRadhakrishnan@users.noreply.github.com>
2024-02-01 13:53:32 +0530

    Update views.py

--- latha1/views.py
+++ latha1/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from django.conf import settings
 
 def hello_world(request):
-   # Settings with env
+  
    # name = settings.NAME
    # return HttpResponse(f"Your name is : {name}")
     return HttpResponse("Hello, World!")
