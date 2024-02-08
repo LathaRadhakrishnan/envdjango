7d3d0162f2335d4d5047a2388529388ae02856a8
LathaRadhakrishnan <114051430+LathaRadhakrishnan@users.noreply.github.com>
2024-02-08 15:13:15 +0530

    Update views.py

--- latha1/views.py
+++ latha1/views.py
@@ -6,12 +6,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 
-def hello_world(request):
-    
 
-    name = settings.NAME
-    return HttpResponse(f"Your name is : {name}")
-   
+from django.http import HttpResponseServerError
+import psutil
+
+def throw_internal_server_error(request):
+    # Simulate an internal server error
+    raise Exception("Internal Server Error")
+
+def consume_memory(request):
+    # Consume memory until reaching a specified limit
+    memory_limit = 100000000  # 100 MB
+    data = bytearray(memory_limit)
+    return HttpResponseServerError("Memory Limit Exceeded")
+
+def use_cpus(request):
+    # Use all available CPUs
+    cpu_count = psutil.cpu_count()
+    for _ in range(cpu_count):
+        psutil.cpu_percent(interval=1)
+    return HttpResponseServerError("CPU Usage Exceeded")
 
 
 
